6609e6af8bd67b3bf8f7f4c7c0e66650880b70a3
tomdaniels <thomsondaniels@hotmail.com>
2022-01-18 18:37:49 +1100

    init nextjs tsconfig & emotion

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import * as styles from '../styles/common.style.js'
+
+const Visualeyes: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      hello world
+    </div>
+  )
+}
+
+export default Visualeyes;
