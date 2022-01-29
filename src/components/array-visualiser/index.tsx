f9cbec3aa1181c02c1966ad0e0e9bd682b6dd6b6
tomdaniels <thomsondaniels@hotmail.com>
2022-01-29 12:11:44 +1100

    style merge sort aniimations

--- src/components/array-visualiser/index.tsx
+++ src/components/array-visualiser/index.tsx
@@ -1,8 +1,13 @@
 import { ReactElement, useState, useEffect } from 'react';
 import randomIntBetween from 'src/utils/random-int-between';
-import mergeSort from 'src/algo/merge-sort';
+import animateMergeSort from 'src/algo/merge-sort';
 
 import * as styles from '../../styles/array-visualiser.style';
+
+const PRIMARY_COLOR = 'red';
+const SECONDARY_COLOR = 'black';
+const NUMBER_OF_ARRAY_BARS = 120;
+const ANIMATION_SPEED_MS = 20;
 
 export default function ArrayVisualiser(): ReactElement {
   const [isMounted, setIsMounted] = useState<boolean>(false);
@@ -17,7 +22,7 @@ export default function ArrayVisualiser(): ReactElement {
 
   function resetArray(): void {
     const newArray = [];
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
       newArray.push(randomIntBetween(20, 1000));
     }
 
@@ -25,21 +30,40 @@ export default function ArrayVisualiser(): ReactElement {
   }
 
   function handleClick(array: number[]) {
-    const result = mergeSort(array);
+    const animations = animateMergeSort(array);
 
-   result.animations.forEach(animation => {
-     console.log('to animate', animation.pair, animation.committed);
-   })
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = Array.from(
+        document.getElementsByClassName(
+          'array-bar'
+        ) as HTMLCollectionOf<HTMLElement>
+      );
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
   }
 
   return (
     <>
       <button onClick={() => handleClick(array)}>sort!</button>
       <div css={styles.grid}>
-        {array.map((elem, idx) => (
-          <div id={`${elem}`} key={idx} css={styles.element(elem)}>
-            {elem}
-          </div>
+        {array.map((value, idx) => (
+          <div className="array-bar" css={styles.element(value)} key={idx} />
         ))}
       </div>
     </>
