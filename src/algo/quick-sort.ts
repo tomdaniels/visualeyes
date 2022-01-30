77babbcb1817e134a5286ba3f5a2778982922e92
tomdaniels <thomsondaniels@hotmail.com>
2022-01-30 16:42:37 +1100

    track start & end walls to animate pivot

--- src/algo/quick-sort.ts
+++ src/algo/quick-sort.ts
@@ -24,11 +24,14 @@ function pivot(
   for (let i = start + 1; i <= end; i++) {
     if (pivot > arr[i]) {
       swapIdx++;
-      swap(arr, swapIdx, i, animations);
+      animations.push([start, end, swapIdx, i]);
+      animations.push([start, end, swapIdx, i]);
+      swap(arr, swapIdx, i, animations, start, end);
     }
   }
-  // animations.push([start, swapIdx]);
-  swap(arr, start, swapIdx, animations);
+  animations.push([start, end, start, swapIdx]);
+  animations.push([start, end, start, swapIdx]);
+  swap(arr, start, swapIdx, animations, start, end);
   return swapIdx;
 }
 
@@ -36,14 +39,12 @@ function swap(
   x: number[],
   idx: number,
   idj: number,
-  animations: any[]
+  animations: any[],
+  start: number,
+  end: number
 ): number[] {
   const temp = x[idx];
-  // push twice to toggle colours
-  animations.push([idx, idj]);
-  animations.push([idx, idj]);
-  // push to commit the new values at each position
-  animations.push([idx, x[idj], idj, x[idx]]);
+  animations.push([start, end, idx, x[idj], idj, x[idx]]);
 
   // actually swap
   x[idx] = x[idj];
