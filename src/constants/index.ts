ba53f180377b6a0a7697af8aa9ba70ca6438d18b
tomdaniels <thomsondaniels@hotmail.com>
2022-01-30 11:28:51 +1100

    update default sample size

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export { default as THEME } from './colours';
 export const ANIMATION_SPEED_MS: number = 10;
-export const NUMBER_OF_ARRAY_BARS: number = 125;
+export const NUMBER_OF_ARRAY_BARS: number = 80;
