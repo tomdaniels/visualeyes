4bcf870834a8befcd47f0960274ee71b5c83a67d
tomdaniels <thomsondaniels@hotmail.com>
2022-01-31 17:10:08 +1100

    simplify pivot node animation

--- src/algo/quick-sort.ts
+++ src/algo/quick-sort.ts
@@ -24,14 +24,14 @@ function pivot(
   for (let i = start + 1; i <= end; i++) {
     if (pivot > arr[i]) {
       swapIdx++;
-      animations.push([start, end, swapIdx, i]);
-      animations.push([start, end, swapIdx, i]);
-      swap(arr, swapIdx, i, animations, start, end);
+      animations.push([start, swapIdx, i]);
+      animations.push([start, swapIdx, i]);
+      swap(arr, swapIdx, i, animations);
     }
   }
-  animations.push([start, end, start, swapIdx]);
-  animations.push([start, end, start, swapIdx]);
-  swap(arr, start, swapIdx, animations, start, end);
+  animations.push([start, start, swapIdx]);
+  animations.push([start, start, swapIdx]);
+  swap(arr, start, swapIdx, animations);
   return swapIdx;
 }
 
@@ -40,11 +40,9 @@ function swap(
   idx: number,
   idj: number,
   animations: any[],
-  start: number,
-  end: number
 ): number[] {
   const temp = x[idx];
-  animations.push([start, end, idx, x[idj], idj, x[idx]]);
+  animations.push([idx, x[idj], idj, x[idx]]);
 
   // actually swap
   x[idx] = x[idj];
